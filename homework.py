e1e4150ded67141b1f952c0253e71956c97ec60e
Overcrewt <35493814+Overcrewt@users.noreply.github.com>
2020-01-21 21:43:33 +0200

    Init

---
+++ homework.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from requests import *
+
+userCode
+
+
+driver = webdriver.Firefox()
+driver.get("https://mcdonalds.fast-insight.com/voc/ee/en")
+input = driver.find_element_by_id('receiptCode')
+button = driver.find_element_by_tag_name('button')
+input.send_keys(userCode)
