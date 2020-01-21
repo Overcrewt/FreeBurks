8d444c4277d7133c71cd8b73d95c51b2897ac50a
Overcrewt <35493814+Overcrewt@users.noreply.github.com>
2020-01-21 22:02:52 +0200

    driver + scrypt

--- homework.py
+++ homework.py
@@ -4,7 +4,7 @@ from requests import *
 userCode
 
 
-driver = webdriver.Firefox()
+driver = webdriver.Firefox("/home/overcrewt/geckodriver")
 driver.get("https://mcdonalds.fast-insight.com/voc/ee/en")
 input = driver.find_element_by_id('receiptCode')
 button = driver.find_element_by_tag_name('button')
